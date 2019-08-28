0092577656e27d5bc8b0784507a629de7ab1557c
YasushiKobayashi <ptpadan@gmail.com>
2019-08-28 16:42:13 +0900

    add codecov cover readme

--- .circleci/artifacts.sh
+++ .circleci/artifacts.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 npx lerna exec -- mkdir -p $CIRCLE_TEST_ARTIFACTS/$(cat package.json \
-| grep name \
-| head -1 \
-| awk -F: '{ print $2 }' \
-| sed 's/[",]//g' \
-| tr -d '[[:space:]]');
+  | grep name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
 npx lerna exec -- cp test-cover.html $CIRCLE_TEST_ARTIFACTS/$(cat package.json \
-| grep name \
-| head -1 \
-| awk -F: '{ print $2 }' \
-| sed 's/[",]//g' \
-| tr -d '[[:space:]]');
+  | grep name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
 npx lerna exec -- cp analyze.txt $CIRCLE_TEST_ARTIFACTS/$(cat package.json \
-| grep name \
-| head -1 \
-| awk -F: '{ print $2 }' \
-| sed 's/[",]//g' \
-| tr -d '[[:space:]]')
+  | grep name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [![CircleCI](https://circleci.com/gh/YasushiKobayashi/js-modules.svg?style=svg)](https://circleci.com/gh/YasushiKobayashi/js-modules)
+
+[![codecov](https://codecov.io/gh/YasushiKobayashi/js-modules/branch/master/graph/badge.svg)](https://codecov.io/gh/YasushiKobayashi/js-modules)
