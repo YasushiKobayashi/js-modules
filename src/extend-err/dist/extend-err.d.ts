28c92558c66de24e12491ffceb41e64e66d1ed79
YasushiKobayashi <ptpadan@gmail.com>
2019-12-30 17:05:59 +0900

    Fix gitignore

--- src/extend-err/dist/extend-err.d.ts
+++
@@ -1,9 +0,0 @@
-interface ExtendErrInterface extends Error {
-    errorStack?: Error;
-}
-declare abstract class ExtendErr extends Error implements ExtendErrInterface {
-    errorStack?: Error;
-    constructor(message?: string | Error, error?: Error);
-}
-
-export default ExtendErr;
